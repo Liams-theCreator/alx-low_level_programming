90ba4f4f53ebb2664d421b68a0e0f3470ddafea1
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-07 08:49:04 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 /**
- * This is the beginnig of main function
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  * */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    return (0);
 }
