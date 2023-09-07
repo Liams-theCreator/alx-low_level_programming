49aab3683aa1166d4cf68d2d43a57ac038d9e254
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-07 08:54:30 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Awlays 0 (Success)
+ */
+int main(void){
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
