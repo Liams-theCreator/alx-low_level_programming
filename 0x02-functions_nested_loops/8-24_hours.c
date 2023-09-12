e078498425690c9546cd65c1386e77b2ea3c764b
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-12 17:40:31 +0100

    a really long day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of
+ *
+ * starting from 00:00 to 23:59.
+ */
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar('0' + hours / 10);
+			_putchar('0' + hours % 10);
+			_putchar(':');
+			_putchar('0' + minutes / 10);
+			_putchar('0' + minutes % 10);
+			_putchar('\n');
+		}
+	}
+}
