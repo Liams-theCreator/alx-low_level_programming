94b7eec8248cb4b245cb970f653b57c2f0d10052
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-12 15:56:13 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * return : void
+ */
+
+void print_alphabet(void)
+{
+
+    char x;
+
+    for (x = 'a'; x <= 'z'; x++)
+    {
+        putchar(x);
+    }
+    putchar('\n');
+}
