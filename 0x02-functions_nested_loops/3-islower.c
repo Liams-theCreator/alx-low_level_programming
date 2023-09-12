78e2359b418ed73f1b14b4ad0c3eb993651399ba
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-12 16:44:22 +0100

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * checks loweracase char
+ *
+ * return : 1 if c lowercase, else , return 0;
+ */
+
+int _islower(int c)
+{
+
+    if (c >= 98 && c <= 122)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
