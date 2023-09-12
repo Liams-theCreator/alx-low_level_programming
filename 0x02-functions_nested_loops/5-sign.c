884b877adffd62e7de4bcc7d626e4947e61ec9e6
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-12 17:05:29 +0100

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _print_sign - Entry point
+ * @n : printed int
+ * print n and check if its greater,less than 0 or equal to 0
+ *
+ * Return: if greater than zero return 1
+ * if equal to zero return 0
+ * and if less than zero return -1
+ *
+ */
+
+int print_sign(int n)
+{
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else
+    {
+        _putchar('-');
+        return (-1);
+    }
