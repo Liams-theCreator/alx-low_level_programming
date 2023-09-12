e9736830a8490f985e47678f207322557a725d13
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-12 16:38:25 +0100

    islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,20 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Entry point
+ * _islower - Entry point
+ * checks loweracase char
  *
- * return : 0
+ * return : 1 if c lowercase, else , return 0;
  */
-void print_alphabet_x10(void)
+
+int _islower(int c)
 {
 
-	char x;
-	int n = 0;
-
-	while (n <= 9)
+	if (c >= 98 && c <= 122)
 	{
-		for (x = 'a'; x <= 'z'; x++)
-		{
-			putchar(x);
-		}
-		putchar('\n');
-		n++;
+		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
