810cc05a2d6a58aa3ce9ca9a44e363546e5c0237
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-12 15:59:22 +0100

    Head

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,17 @@
 #include <stdio.h>
 
 void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
+int add(int a, int b);
+void print_times_table(int n);
 
 #endif
