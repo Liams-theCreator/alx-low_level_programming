0f522a7a55ef04a8bdb2d2cc05c068509eed946c
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-12 16:48:17 +0100

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,18 +4,19 @@
  * @c : printed char
  * checks loweracase char
  *
- * return : 1 if c lowercase, else , return 0;
+ * return : 1 if c lowercase
+ * and 0 if there is an error
  */
 
 int _islower(int c)
 {
 
-    if (c >= 98 && c <= 122)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (c >= 98 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
