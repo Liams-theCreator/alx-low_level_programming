48b4e31989bd18d2171e52b61109bcef9f0676a8
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-14 17:42:16 +0100

    Speed run diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,10 +6,11 @@
 
 void print_diagonal(int n)
 {
+	int i, j;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (int j = 0; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
