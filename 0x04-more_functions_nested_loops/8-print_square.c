b9f393f11af08df4e3a506df99d7f9d8791716c7
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-14 17:49:30 +0100

    nunu square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,16 +7,17 @@
 
 void print_square(int size)
 {
-    int i, j;
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
-    if (size <= 0)
-        _putchar('\n');
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
 }
