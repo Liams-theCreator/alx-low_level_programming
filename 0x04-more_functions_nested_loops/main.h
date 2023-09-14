bf8049f7c05d60f37707b373b902ad0499c09eed
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-14 16:56:24 +0100

    Fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char);
-int _isupper(char c);
-int _isdigit(char c);
-int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
 #endif /* MAIN_H */
