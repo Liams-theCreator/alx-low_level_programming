28031357b40da8786617ffd13c5b8702e2beee52
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-18 15:46:52 +0100

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character
+ *
+ * @str: char
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
