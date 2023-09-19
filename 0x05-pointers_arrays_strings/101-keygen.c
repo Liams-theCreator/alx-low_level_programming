f11d92680ef1f64dc3a1fccc9f930b4bd1dd9e0e
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-19 17:58:13 +0100

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random passwords 101-crackme
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+
+  int sum = 0;
+  char random;
+
+  srand(time(NULL));
+  while (sum <= 2772)
+  {
+    random = rand() % 128;
+    sum += random;
+    putchar(random);
+  }
+  putchar(2772 - sum);
+
+  return (0);
+}
