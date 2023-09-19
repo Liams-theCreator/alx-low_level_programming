5c8b62e042ae4d7e91d2987bb1d2004cbe656cc1
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-19 15:46:12 +0100

    ASCII to Integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -34,7 +34,7 @@ int _atoi(char *s)
 
 		while (s[i] >= '0' && s[i] <= '9')
 		{
-			result = result * 10 + (s[i] - '0'); // Corrected the conversion
+			result = result * 10 + (s[i] - '0');
 			i++;
 		}
 	}
