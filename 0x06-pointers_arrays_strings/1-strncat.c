71497b7bdd29e2d1387f3658b330f15193839067
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-20 16:14:40 +0100

    Strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,16 +12,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i = 0;
+	int j = 0;
 
-	for (; dest[i] != '\0'; i++)
+	while (dest[i] != '\0')
 	{
-		continue;
+		i++;
 	}
 
-	for (; src[j] != '\0' && j < n; j++)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
+		j++;
 		i++;
 	}
 
