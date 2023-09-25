8af507e33e4a1283f51979878ad50414df47ece0
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-25 16:38:32 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,14 +18,10 @@ char *_strstr(char *haystack, char *needle)
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (needle[j] != haystack[i + j])
-			{
 				break;
-			}
 		}
 		if (needle[j] == '\0')
-		{
 			return (haystack + i);
-		}
 	}
 	return (NULL);
 }
