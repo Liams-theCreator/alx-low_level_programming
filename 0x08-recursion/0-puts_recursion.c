31ccde66dab77054e3126d9cae7a8cc49bda54f9
Liams-theCreator <mmeellaallii1@gmail.com>
2023-09-28 14:53:21 +0100

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: pointer to string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	return;
+	_putchar('\n');
+}
