40cd9638ba323162a47e8b71b6d67bd770f44e0b
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-03 14:36:05 +0100

    first block of memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
-	int i;
+	unsigned int i;
 
 	s = malloc(size * sizeof(char));
 
 	if (s == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
