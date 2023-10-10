2e9c596ba1d133f0bf4cf41111bdf7e96f979ab4
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-11 00:08:11 +0100

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * free_dog - frees dogs from captivity
+ * @d: pointer to the captive dogs
+ */
+
+void free_dog(dog_t *d)
+{
+ if (!d)
+		return;
+
+ free(d->name);
+ free(d->owner);
+ free(d);
+}
