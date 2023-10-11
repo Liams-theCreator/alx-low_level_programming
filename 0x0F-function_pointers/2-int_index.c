fa7eb9cdeed5faad8bd16b5c662e7220178d610e
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-11 19:44:54 +0100

    index of element

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,15 +12,21 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	int i;
 
-	if (array == NULL || cmp == NULL || size < 0)
+	if (size <= 0)
 	{
 		return (-1);
 	}
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) == 1)
+		if (cmp(array[i]))
 		{
 			return (i);
 		}
