4b8256583baeb58534cf9e3c1aa53102043f7e3f
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-11 19:02:17 +0100

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,10 +14,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 	{
-		return;
-	}
-	for (i = 0; i < size; i++)
-	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
