f061f29ec837cd09aeda263c5c35693613dd1d4b
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-12 13:30:20 +0100

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if (separator != NULL)
 		{
-			printf("%c", separator);
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
