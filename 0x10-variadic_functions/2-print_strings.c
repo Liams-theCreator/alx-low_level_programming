a29bede308bc020861d2066f6b40495f709fc214
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-12 14:10:15 +0100

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(str, n);
 	for (i = 0; i < n; i++)
 	{
-		string = va_arg(str, *char);
+		string = va_arg(str, char *);
 
 		if (string == NULL)
 		{
