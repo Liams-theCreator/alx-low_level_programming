aaeab87b75f0eb3b45044a98fbe6b89aab2cd234
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-19 17:42:51 +0000

    Fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,6 +21,5 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-
 
 #endif
