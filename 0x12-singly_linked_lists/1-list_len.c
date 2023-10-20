48be64121e01ff424de9daf5febcb1da0eab45cc
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-20 00:50:02 +0000

    list lenghth

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,12 +10,12 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t len = 0;
+	size_t i = 0;
 
-	for (; h != NULL; h = h->next)
+	while (h)
 	{
-		len++;
+		h = h->next;
+		i++;
 	}
-
-	return (len);
+	return (i);
 }
