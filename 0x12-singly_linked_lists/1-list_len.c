fcefacaae687dfcf0e1e1d0c0f2253446c6ce4fe
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-20 00:49:16 +0000

    list lenghth

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <lists.h>
+
+/**
+ * list_len - Counts the number of nodes in a linked list.
+ *
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		len++;
+	}
+
+	return (len);
+}
