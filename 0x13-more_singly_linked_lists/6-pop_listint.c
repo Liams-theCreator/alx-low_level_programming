30c33d20f17c998695859640f4e38ff7514fbe1a
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-24 17:31:37 +0100

    Pop smoke

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,15 @@
 #include "lists.h"
 
+/**
+ * pop_listint - Deletes the head node of a listint_t
+ * linked list and returns its data.
+ *
+ * @head: Pointer to a pointer to the head of the list.
+ *
+ * Return: The data (n) of the deleted head node,
+ *  or 0 if the list is empty.
+ */
+
 int pop_listint(listint_t **head)
 {
 	listint_t *temporary = *head;
