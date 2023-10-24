fda7163898ba9ec2c6deda6c687fd6cc1ce63d3b
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-24 17:34:45 +0100

    Pop smoke

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,13 +13,12 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temporary = *head;
+	int data = temporary->n;
 
 	if (*head == NULL)
 	{
 		return (0);
 	}
-
-	int data = temporary->n;
 
 	*head = (*head)->next;
 	free(temporary);
