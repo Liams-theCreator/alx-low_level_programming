9bdf839746f954efc0679092a90e72256155dfff
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-27 00:47:45 +0100

    Binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,19 +15,17 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 	{
-		return (0);
+		return 0;
 	}
 
-	for (; b[i] != '\0'; i++)
+	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
-			return (0);
+			return 0;
 		}
-		else
-		{
-			result = (b[i] << 1) + (b[i] - '0');
-		}
+		result = (result << 1) + (b[i] - '0');
+		i++;
 	}
 	return (result);
 }
