928376f9afd057fea38a8addbe97a897c89a91ae
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-27 01:33:43 +0100

    Bit by bit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * flip_bits - Returns the number of bits to flip to get from n to m.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits to flip.
+ */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int xor = n ^ m;
