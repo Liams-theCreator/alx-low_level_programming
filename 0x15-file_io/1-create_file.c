e52e4645b2ebbede9aee7a91c8548ef2880dc981
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-31 01:51:09 +0100

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
