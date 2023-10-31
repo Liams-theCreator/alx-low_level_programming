d1ec6edb0ad5bf48b00750dd2c5b5df65e154962
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-31 01:22:58 +0100

    Create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * _strlen - count length of string
+ *
+ * @s: pointer to char
+ *
+ * Return: return the length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * create_file - Create a file and write text content to it.
+ *
+ * @filename: Name of the file to create or truncate.
+ * @text_content: Text content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int len, fd;
+	ssize_t write_byte;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+	if (fp == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		write_byte = write(fd, text_content, _strlen(text_content));
+		if (write_byte == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
