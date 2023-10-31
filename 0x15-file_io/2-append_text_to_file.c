6647a47aa59ac44997dbc8b96a482143bae21c0a
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-31 02:18:22 +0100

    Append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -39,7 +39,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
 		return (-1);
