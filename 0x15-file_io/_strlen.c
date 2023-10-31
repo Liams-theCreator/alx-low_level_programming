e27a51fa2d73810ee874a5323fdb29f4cca8a036
Liams-theCreator <mmeellaallii1@gmail.com>
2023-10-31 01:27:49 +0100

    strlen

---
+++ 0x15-file_io/_strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - count length of string
+ *
+ * @s: pointer to char
+ *
+ * Return: return the length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
