fd331881c01cd7a6c1e6450a35c7f98a8b496d15
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-07 15:05:18 +0100

    Printing

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,11 +10,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	while (node != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", node->n);
+		printf("%d\n", h->n);
 		count++;
-		node = node->next;
+		h = h->next;
 	}
 	return (count);
 }
