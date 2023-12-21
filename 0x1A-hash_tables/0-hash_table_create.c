2f75203a97929a712f1536fd1825745817ac12e5
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-21 18:22:14 +0100

    Create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hashtable.h"
+
+/*
+ * hash_table_create - Create a hash table.
+ * @size: Size of the array in the hash table.
+ *
+ * Return: A pointer to the newly created hash table,
+ *         or NULL if an error occurs.
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *tableau = NULL;
+
+	tableau = malloc(sizeof(hash_table_t));
+	if (tableau == NULL)
+	{
+		return (NULL);
+	}
+
+	tableau->size = size;
+	tableau->array = calloc(size, sizeof(hash_node_t));
+	if (tableau->array == NULL)
+	{
+		return (NULL);
+	}
+	return (tableau);
+}
