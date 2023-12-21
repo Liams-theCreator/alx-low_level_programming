761046e343bad57bdeca2f76c7f829c25859ec8a
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-21 23:18:35 +0100

    Get Hash Table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,14 +9,15 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((unsigned char *)key, ht->size); /*neccessary to cast !?*/
-	hash_node_t *node = ht->array[index];
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *node;
 
 	if (!ht || !key || !key[0])
 	{
 		return (NULL);
 	}
 
+	node = ht->array[index];
 	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
