e851d6eac395876c0bf3abc5d02d395ece6453db
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-21 23:05:13 +0100

    Set Hash Table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	index = key_index(key, ht->size);
+	index = key_index((unsigned char *)key, ht->size); /* neccessary to cast !?*/
 	node = ht->array[index];
 	while (node) /* klimogin check if key found update it*/
 	{
