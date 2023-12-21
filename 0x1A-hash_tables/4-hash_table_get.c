86b2184c74918650d28d8cd0e3174b2ff25dc5bf
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-21 23:09:54 +0100

    Get Hash Table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: pointer to the hash table
+ * @key: key string
+ * Return: value associated with the key, or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key || !key[0])
+	{
+		return (NULL);
+	}
+
+	index = key_index(key, ht->size);
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+
+	return (NULL);
+}
