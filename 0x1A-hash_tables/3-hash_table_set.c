45655c738d84837a5db2798710514691005e97fa
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-21 22:54:12 +0100

    Set Hash Table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,13 +10,16 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index(key, ht->size);
-	hash_node_t *node = ht->array[index];
+	unsigned long int index;
+	hash_node_t *node;
 
 	if (!ht || !key || !key[0])
 	{
-		return (0);
+		return 0;
 	}
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
 	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
@@ -27,14 +30,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		node = node->next;
 	}
-	node = malloc(sizeof(hash_node_t));
+	node = malloc(sizeof(hash_table_t));
 	if (node == NULL)
 	{
 		return (0);
 	}
 	node->key = strdup(key);
 	node->value = strdup(value);
-	node->next = ht->array[index]; /* to biginin */
+	node->next = ht->array[index];
 	ht->array[index] = node;
 
 	return (1);
