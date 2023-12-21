84a1de6b165ee03edd81d21de2b5f603a59fe935
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-21 18:32:43 +0100

    Create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	tableau->array = calloc(size, sizeof(hash_node_t));
 	if (tableau->array == NULL)
 	{
-		free(tableau->array);
+		free(tableau);
 		return (NULL);
 	}
 	return (tableau);
