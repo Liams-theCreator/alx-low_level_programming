8d335faf7788035b978124f9723c7a7ccf57fd28
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-22 22:49:05 +0100

    Print Hash Table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: Hash table
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i = 0, size = ht->size;
+	int fasila = 0;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (; i < size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (fasila != 0)
+			{
+				printf(", ");
+			}
+			printf("'%s': '%s'", node->key, node->value);
+			fasila = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
